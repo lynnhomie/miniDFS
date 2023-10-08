99eccd424d319bf15a3277c793acb84543f9337b
lynnhomie <16603810930@163.com>
2023-10-08 15:15:55 +0800

    Update nameserver.h

--- nameserver.h
+++ nameserver.h
@@ -12,8 +12,8 @@ private:
     FileTree fileTree_;
     int numReplicate_;
     int idCnt_;
-
     std::vector<std::string> parse_cmd();
+    std::map<std::string,bool>heartbeatStatus_; //记录数据服务器与其心跳状态的对应关系
 public:
     std::map<std::string, std::pair<int, int>> meta;
 
