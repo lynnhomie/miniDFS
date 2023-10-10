701c710536a1972b2bb6d7a9e9b144146b7a2d4d
lynnhomie <16603810930@163.com>
2023-10-10 09:49:30 +0800

    Update dataserver.cpp

--- dataserver.cpp
+++ dataserver.cpp
@@ -98,12 +98,9 @@ void DataServer::locate(){
         bufSize = 0;
 }
 
-void DataServer::sendHeartbeat(NameServer* nameServer){
+void DataServer::sendHeartbeat(){
     while(true){
-        //发送心跳信号给nameserver
-        nameServer->receiveHeartbeat(ServerID(name_),true);
-        //每隔10s发送一次
-        std::this_thread::sleep_for(std::chrono::seconds(10));
+    
     }
 
 }
