5fc4c04a40b7f2f3885f95f75d0233d0d9988760
lynnhomie <16603810930@163.com>
2023-10-20 10:57:46 +0800

    Update nameserver.h

--- nameserver.h
+++ nameserver.h
@@ -1,26 +1,28 @@
-#ifndef _NAME_SERVER_H
+#ifndef _fNAME_SERVER_H
 #define _NAME_SERVER_H
 
 #include <vector>
-#include <map>
 #include "dataserver.h"
 #include "filetree.h"
 
 class NameServer{
 private:
-    std::vector<DataServer *> dataServers_;
+    
     FileTree fileTree_;
     int numReplicate_;
     int idCnt_;
     std::vector<std::string> parse_cmd();
-    std::map<std::string,bool>heartbeatStatus_; //记录数据服务器与其心跳状态的对应关系
 public:
+    static std::mutex mtx1;
     std::map<std::string, std::pair<int, int>> meta;
-
+    std::vector<DataServer *> dataServers_;
     explicit NameServer(int numReplicate);
     void add(DataServer *server);
     void operator()();
+    void heartbeatProcessing();
+    
+    
+    
 };
-
 
 #endif
